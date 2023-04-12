c5b4775659d013bbe007445fc6f8ed947de01f75
Saltanova Zhanna <nedikovazhanna@gmail.com>
2023-04-12 16:33:46 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Task06Component } from './task06/task06.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Task06Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
